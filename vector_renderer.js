be96af2d9c2a521443f0a86944b06ebba08b1147
Brett Camper <bcamper@alum.mit.edu>
2013-12-01 15:38:44 -0500

    alternate dev server URL

--- vector_renderer.js
+++ vector_renderer.js
@@ -11,7 +11,8 @@ VectorRenderer.prototype.loadTile = function (coords, div)
     // Load tile
     var tile_url = 'http://tile.openstreetmap.us/vectiles-all/' + coords.z + '/' + coords.x + '/' + coords.y + '.json';
     // var tile_url = 'http://api-vector-test.mapzen.com/vector/all/' + coords.z + '/' + coords.x + '/' + coords.y + '.json';
-    // var tile_url = 'http://localhost:8080/vector/all/' + coords.z + '/' + coords.x + '/' + coords.y + '.json';
+    // var tile_url = 'http://localhost:8080/vector/all/' + coords.z + '/' + coords.x + '/' + coords.y + '.json'; // local mapzenuff vagrant server
+    // var tile_url = 'http://localhost:8080/all/' + coords.z + '/' + coords.x + '/' + coords.y + '.json'; // local tilestache server
 
     var key = [coords.x, coords.y, coords.z].join('/');
     var timer = +new Date();
