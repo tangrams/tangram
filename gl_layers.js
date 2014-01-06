7f50ae840cf35747b1c7b286e3b69637a5821d2d
Brett Camper <bcamper@alum.mit.edu>
2014-01-05 18:36:09 -0800

    sorting update on layers

--- gl_layers.js
+++ gl_layers.js
@@ -15,8 +15,15 @@
        data: function (json) { return json['land-areas']; }
     },
     {
-       name: 'land_usages',
-       data: function (json) { return json['land-usages']; }
+        name: 'land_usages',
+        data: function (json) {
+            return {
+                type: 'FeatureCollection',
+                features: json['land-usages'].features.sort(function(a, b) {
+                    return (b.properties.area - a.properties.area); // order roads by prodivded polygon area
+                })
+            };
+        }
     },
     {
        name: 'water_areas',
@@ -32,27 +39,12 @@
     {
         name: 'roads',
         data: function (json) {
-            // Order roads using provided 'sort_key'
-            // var data = {
-            //     type: 'FeatureCollection',
-            //     features: json['highroad'].features.sort(function(a, b) {
-            //         return (a.properties.sort_key - b.properties.sort_key);
-            //     })
-            // };
-
-            // Normalize sort keys (there are some really high and low values that cause too much spatial separation of layers)
-            // var i = null, last_sort_key = null;
-            // for (var f in data.features) {
-            //     var sort_key = data.features[f].properties.sort_key;
-            //     if (sort_key != last_sort_key) {
-            //         i = (i + 1) || 0;
-            //         last_sort_key = sort_key;
-            //     }
-            //     data.features[f].properties.sort_key = i;
-            // };
-
-            // return data;
-            return json['highroad'];
+            return {
+                type: 'FeatureCollection',
+                features: json['highroad'].features.sort(function(a, b) {
+                    return (a.properties.sort_key - b.properties.sort_key); // order roads by prodivded 'sort_key'
+                })
+            };
         }
     },
     {
