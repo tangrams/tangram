078368582356b16afc0312e3f6b65688d4bc2168
Brett Camper <bcamper@alum.mit.edu>
2014-05-09 12:24:35 -0400

    guard against missing layers, use shortcut when no post-processing is needed for a layer

--- gl_layers_json.js
+++ gl_layers_json.js
@@ -1,22 +1,30 @@
 [
     {
-       name: 'water_ocean',
-       data: function (json) {
-           return {
-               type: 'FeatureCollection',
-               features: json['water-areas'].features.filter(function (feature) {
-                   return feature.properties.kind == 'ocean';
-               })
-           };
-       }
+        name: 'water_ocean',
+        data: function (json) {
+            if (!json['water-areas'] || !json['water-areas'].features) {
+                return null;
+            }
+
+            return {
+                type: 'FeatureCollection',
+                features: json['water-areas'].features.filter(function (feature) {
+                    return feature.properties.kind == 'ocean';
+                })
+            };
+        }
     },
     {
-       name: 'land_areas',
-       data: function (json) { return json['land-areas']; }
+        name: 'land_areas',
+        data: 'land-areas'
     },
     {
         name: 'land_usages',
         data: function (json) {
+            if (!json['land-usages'] || !json['land-usages'].features) {
+                return null;
+            }
+
             return {
                 type: 'FeatureCollection',
                 features: json['land-usages'].features.sort(function(a, b) {
@@ -26,29 +34,37 @@
         }
     },
     {
-       name: 'water_areas',
-       data: function (json) {
-           return {
-               type: 'FeatureCollection',
-               features: json['water-areas'].features.filter(function (feature) {
-                   return feature.properties.kind != 'ocean';
-               })
-           };
-       }
+        name: 'water_areas',
+        data: function (json) {
+            if (!json['water-areas'] || !json['water-areas'].features) {
+                return null;
+            }
+
+            return {
+                type: 'FeatureCollection',
+                features: json['water-areas'].features.filter(function (feature) {
+                    return feature.properties.kind != 'ocean';
+                })
+            };
+        }
     },
     {
         name: 'roads',
         data: function (json) {
+            if (!json['highroad'] || !json['highroad'].features) {
+                return null;
+            }
+
             return {
                 type: 'FeatureCollection',
                 features: json['highroad'].features.sort(function(a, b) {
-                    return (a.properties.sort_key - b.properties.sort_key);
+                  return (a.properties.sort_key - b.properties.sort_key);
                 })
             };
         }
     },
     {
         name: 'buildings',
-        data: function (json) { return json['buildings']; }
+        data: 'buildings'
     }
 ]
