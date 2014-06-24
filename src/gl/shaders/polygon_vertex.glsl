e428e31ea21c578086b1e9b9063f5382d3c5582c
Brett Camper <bcamper@alum.mit.edu>
2014-06-23 23:48:09 -0700

    start moving some shader functions to glslify modules

--- src/gl/shaders/polygon_vertex.glsl
+++ src/gl/shaders/polygon_vertex.glsl
@@ -14,6 +14,8 @@ attribute float layer;
 
 varying vec3 fcolor;
 
+#pragma glslify: perspectiveTransform = require(./modules/perspective, resolution=resolution, meter_zoom=meter_zoom)
+
 #if defined(EFFECT_NOISE_TEXTURE)
     varying vec3 fposition;
 #endif
@@ -36,36 +38,6 @@ vec3 modelViewTransform (vec3 position) {
     // NOTE: due to unresolved floating point precision issues, tile and map center adjustment need to happen in ONE operation, or artifcats are introduced
     position.xy += tile_min.xy - map_center; // adjust for corner of tile relative to map center
     position.xy /= meter_zoom; // adjust for zoom in meters to get clip space coords
-
-    return position;
-}
-
-vec3 perspectiveTransform (vec3 position) {
-    #if defined(PROJECTION_PERSPECTIVE)
-        // Perspective-style projection
-        const vec2 perspective_offset = vec2(-0.25, -0.25);
-        const vec2 perspective_factor = vec2(0.8, 0.8); // vec2(-0.25, 0.75);
-        position.xy += position.z * perspective_factor * (position.xy - perspective_offset) / meter_zoom.xy; // perspective from offset center screen
-    #elif defined(PROJECTION_ISOMETRIC) || defined(PROJECTION_POPUP)
-        // Pop-up effect - 3d in center of viewport, fading to 2d at edges
-        #if defined(PROJECTION_POPUP)
-            if (position.z > 1.0) {
-                float cd = distance(position.xy * (resolution.xy / resolution.yy), vec2(0.0, 0.0));
-                const float popup_fade_inner = 0.5;
-                const float popup_fade_outer = 0.75;
-                if (cd > popup_fade_inner) {
-                    position.z *= 1.0 - smoothstep(popup_fade_inner, popup_fade_outer, cd);
-                }
-                const float zoom_boost_start = 15.0;
-                const float zoom_boost_end = 17.0;
-                const float zoom_boost_magnitude = 0.75;
-                position.z *= 1.0 + (1.0 - smoothstep(zoom_boost_start, zoom_boost_end, map_zoom)) * zoom_boost_magnitude;
-            }
-        #endif
-
-        // Isometric-style projection
-        position.y += position.z / meter_zoom.y; // z coordinate is a simple translation up along y axis, ala isometric
-    #endif
 
     return position;
 }
