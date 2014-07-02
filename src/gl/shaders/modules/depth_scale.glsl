7831cda3be828e16d4b42a325f834fcb1f9c961f
Brett Camper <bcamper@alum.mit.edu>
2014-07-02 15:40:12 -0700

    pull z layering scale factor into an external constant

--- src/gl/shaders/modules/depth_scale.glsl
+++ src/gl/shaders/modules/depth_scale.glsl
@@ -1,11 +1,14 @@
+uniform float u_num_layers;
+
+const float z_layer_scale;
+
 float calculateZ (float z, float layer) {
     // Reverse and scale to 0-1 for GL depth buffer
     // Layers are force-ordered (higher layers guaranteed to render on top of lower), then by height/depth
-    const float z_layer_scale = 4096.;
     float z_layer_range = (u_num_layers + 1.) * z_layer_scale;
     float z_layer = (layer + 1.) * z_layer_scale;
 
-    z = z_layer + clamp(z, 1., z_layer_scale);
+    z = z_layer + clamp(z, 0., z_layer_scale);
     z = (z_layer_range - z) / z_layer_range;
 
     return z;
