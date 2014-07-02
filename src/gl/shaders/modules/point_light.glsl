087b3cfa71dcd9569df27f745990ed1a3e1c2301
Brett Camper <bcamper@alum.mit.edu>
2014-07-02 15:41:50 -0700

    split height-based lighting boost into its own function

--- src/gl/shaders/modules/point_light.glsl
+++ src/gl/shaders/modules/point_light.glsl
@@ -1,15 +1,12 @@
 vec3 pointLight (
-    vec3 position,
+    vec4 position,
     vec3 normal,
     vec3 color,
     vec3 light_pos,
-    float light_ambient,
-    float light_height_gradient_factor,
-    float light_height_gradient_max) {
+    float light_ambient) {
 
-    // Point light-based gradient
-    vec3 light_dir = normalize(vec3(position.x, position.y, position.z) - light_pos); // from light point to vertex
-    color *= dot(normal, light_dir * -1.0) + light_ambient + clamp(position.z * light_height_gradient_factor, 0.0, light_height_gradient_max);
+    vec3 light_dir = normalize(position.xyz - light_pos); // from light point to vertex
+    color *= dot(normal, light_dir * -1.0) + light_ambient;
     return color;
 }
 
