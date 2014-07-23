30cf11db776d378453b4d1b3e33dda7ce8f2ffad
Brett Camper <bcamper@alum.mit.edu>
2014-07-23 15:13:02 -0400

    continue to fix/refine YAML stylesheet add support for stylesheet shader snippets to be inline block of GLSL, or a URL to retrieve GLSL block from

--- src/gl/gl_shaders.js
+++ src/gl/gl_shaders.js
@@ -62,111 +62,32 @@ shader_sources['polygon_fragment'] =
 "uniform float u_meters_per_pixel;\n" +
 "uniform float u_time;\n" +
 "varying vec3 v_color;\n" +
+"varying vec4 v_position_world;\n" +
 "#if !defined(LIGHTING_VERTEX)\n" +
 "\n" +
 "varying vec4 v_position;\n" +
 "varying vec3 v_normal;\n" +
 "#endif\n" +
 "\n" +
-"varying vec4 v_position_world;\n" +
-"#if defined(EFFECT_NOISE_TEXTURE)\n" +
-"\n" +
-"vec3 a_x_mod289(vec3 x) {\n" +
-"  return x - floor(x * (1.0 / 289.0)) * 289.0;\n" +
-"}\n" +
-"vec4 a_x_mod289(vec4 x) {\n" +
-"  return x - floor(x * (1.0 / 289.0)) * 289.0;\n" +
-"}\n" +
-"vec4 a_x_permute(vec4 x) {\n" +
-"  return a_x_mod289(((x * 34.0) + 1.0) * x);\n" +
-"}\n" +
-"vec4 a_x_taylorInvSqrt(vec4 r) {\n" +
-"  return 1.79284291400159 - 0.85373472095314 * r;\n" +
-"}\n" +
-"vec3 a_x_fade(vec3 t) {\n" +
-"  return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);\n" +
-"}\n" +
-"float a_x_cnoise(vec3 P) {\n" +
-"  vec3 Pi0 = floor(P);\n" +
-"  vec3 Pi1 = Pi0 + vec3(1.0);\n" +
-"  Pi0 = a_x_mod289(Pi0);\n" +
-"  Pi1 = a_x_mod289(Pi1);\n" +
-"  vec3 Pf0 = fract(P);\n" +
-"  vec3 Pf1 = Pf0 - vec3(1.0);\n" +
-"  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);\n" +
-"  vec4 iy = vec4(Pi0.yy, Pi1.yy);\n" +
-"  vec4 iz0 = Pi0.zzzz;\n" +
-"  vec4 iz1 = Pi1.zzzz;\n" +
-"  vec4 ixy = a_x_permute(a_x_permute(ix) + iy);\n" +
-"  vec4 ixy0 = a_x_permute(ixy + iz0);\n" +
-"  vec4 ixy1 = a_x_permute(ixy + iz1);\n" +
-"  vec4 gx0 = ixy0 * (1.0 / 7.0);\n" +
-"  vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;\n" +
-"  gx0 = fract(gx0);\n" +
-"  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);\n" +
-"  vec4 sz0 = step(gz0, vec4(0.0));\n" +
-"  gx0 -= sz0 * (step(0.0, gx0) - 0.5);\n" +
-"  gy0 -= sz0 * (step(0.0, gy0) - 0.5);\n" +
-"  vec4 gx1 = ixy1 * (1.0 / 7.0);\n" +
-"  vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;\n" +
-"  gx1 = fract(gx1);\n" +
-"  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);\n" +
-"  vec4 sz1 = step(gz1, vec4(0.0));\n" +
-"  gx1 -= sz1 * (step(0.0, gx1) - 0.5);\n" +
-"  gy1 -= sz1 * (step(0.0, gy1) - 0.5);\n" +
-"  vec3 g000 = vec3(gx0.x, gy0.x, gz0.x);\n" +
-"  vec3 g100 = vec3(gx0.y, gy0.y, gz0.y);\n" +
-"  vec3 g010 = vec3(gx0.z, gy0.z, gz0.z);\n" +
-"  vec3 g110 = vec3(gx0.w, gy0.w, gz0.w);\n" +
-"  vec3 g001 = vec3(gx1.x, gy1.x, gz1.x);\n" +
-"  vec3 g101 = vec3(gx1.y, gy1.y, gz1.y);\n" +
-"  vec3 g011 = vec3(gx1.z, gy1.z, gz1.z);\n" +
-"  vec3 g111 = vec3(gx1.w, gy1.w, gz1.w);\n" +
-"  vec4 norm0 = a_x_taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));\n" +
-"  g000 *= norm0.x;\n" +
-"  g010 *= norm0.y;\n" +
-"  g100 *= norm0.z;\n" +
-"  g110 *= norm0.w;\n" +
-"  vec4 norm1 = a_x_taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));\n" +
-"  g001 *= norm1.x;\n" +
-"  g011 *= norm1.y;\n" +
-"  g101 *= norm1.z;\n" +
-"  g111 *= norm1.w;\n" +
-"  float n000 = dot(g000, Pf0);\n" +
-"  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));\n" +
-"  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));\n" +
-"  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));\n" +
-"  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));\n" +
-"  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));\n" +
-"  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));\n" +
-"  float n111 = dot(g111, Pf1);\n" +
-"  vec3 fade_xyz = a_x_fade(Pf0);\n" +
-"  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);\n" +
-"  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);\n" +
-"  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);\n" +
-"  return 2.2 * n_xyz;\n" +
-"}\n" +
-"#endif\n" +
-"\n" +
 "const float light_ambient = 0.5;\n" +
-"vec3 c_x_pointLight(vec4 position, vec3 normal, vec3 color, vec4 light_pos, float light_ambient, const bool backlight) {\n" +
+"vec3 b_x_pointLight(vec4 position, vec3 normal, vec3 color, vec4 light_pos, float light_ambient, const bool backlight) {\n" +
 "  vec3 light_dir = normalize(position.xyz - light_pos.xyz);\n" +
 "  color *= abs(max(float(backlight) * -1., dot(normal, light_dir * -1.0))) + light_ambient;\n" +
 "  return color;\n" +
 "}\n" +
-"vec3 d_x_directionalLight(vec3 normal, vec3 color, vec3 light_dir, float light_ambient) {\n" +
+"vec3 c_x_directionalLight(vec3 normal, vec3 color, vec3 light_dir, float light_ambient) {\n" +
 "  light_dir = normalize(light_dir);\n" +
 "  color *= dot(normal, light_dir * -1.0) + light_ambient;\n" +
 "  return color;\n" +
 "}\n" +
-"vec3 b_x_lighting(vec4 position, vec3 normal, vec3 color, vec4 light_pos, vec4 night_light_pos, vec3 light_dir, float light_ambient) {\n" +
+"vec3 a_x_lighting(vec4 position, vec3 normal, vec3 color, vec4 light_pos, vec4 night_light_pos, vec3 light_dir, float light_ambient) {\n" +
 "  \n" +
 "  #if defined(LIGHTING_POINT)\n" +
-"  color = c_x_pointLight(position, normal, color, light_pos, light_ambient, true);\n" +
+"  color = b_x_pointLight(position, normal, color, light_pos, light_ambient, true);\n" +
 "  #elif defined(LIGHTING_NIGHT)\n" +
-"  color = c_x_pointLight(position, normal, color, night_light_pos, 0., false);\n" +
+"  color = b_x_pointLight(position, normal, color, night_light_pos, 0., false);\n" +
 "  #elif defined(LIGHTING_DIRECTION)\n" +
-"  color = d_x_directionalLight(normal, color, light_dir, light_ambient);\n" +
+"  color = c_x_directionalLight(normal, color, light_dir, light_ambient);\n" +
 "  #else\n" +
 "  color = color;\n" +
 "  #endif\n" +
@@ -177,17 +98,10 @@ shader_sources['polygon_fragment'] =
 "void main(void) {\n" +
 "  vec3 color = v_color;\n" +
 "  #if !defined(LIGHTING_VERTEX) // default to per-pixel lighting\n" +
-"  color = b_x_lighting(v_position, v_normal, color, vec4(0., 0., 150. * u_meters_per_pixel, 1.), vec4(0., 0., 50. * u_meters_per_pixel, 1.), vec3(0.2, 0.7, -0.5), light_ambient);\n" +
+"  color = a_x_lighting(v_position, v_normal, color, vec4(0., 0., 150. * u_meters_per_pixel, 1.), vec4(0., 0., 50. * u_meters_per_pixel, 1.), vec3(0.2, 0.7, -0.5), light_ambient);\n" +
 "  #endif\n" +
 "  \n" +
 "  #pragma tangram: fragment\n" +
-"  \n" +
-"  #if defined(EFFECT_SPOTLIGHT)\n" +
-"  vec2 position = gl_FragCoord.xy / u_resolution.xy;\n" +
-"  position = position * 2.0 - 1.0;\n" +
-"  position *= u_aspect;\n" +
-"  color *= max(1.0 - distance(position, vec2(0.0, 0.0)), 0.2);\n" +
-"  #endif\n" +
 "  gl_FragColor = vec4(color, 1.0);\n" +
 "}\n" +
 "";
--- src/gl/shaders/polygon_fragment.glsl
+++ src/gl/shaders/polygon_fragment.glsl
@@ -5,16 +5,11 @@ uniform float u_meters_per_pixel;
 uniform float u_time;
 
 varying vec3 v_color;
+varying vec4 v_position_world;
 
 #if !defined(LIGHTING_VERTEX)
     varying vec4 v_position;
     varying vec3 v_normal;
-#endif
-
-varying vec4 v_position_world;
-#if defined(EFFECT_NOISE_TEXTURE)
-    // varying vec4 v_position_world;
-    #pragma glslify: cnoise = require(glsl-noise/classic/3d)
 #endif
 
 const float light_ambient = 0.5;
@@ -37,32 +32,6 @@ void main (void) {
     #endif
 
     #pragma tangram: fragment
-
-    // Spotlight effect
-    // TODO: move this snippet to its own file
-    #if defined(EFFECT_SPOTLIGHT)
-        vec2 position = gl_FragCoord.xy / u_resolution.xy;  // scale coords to [0.0, 1.0]
-        position = position * 2.0 - 1.0;                    // scale coords to [-1.0, 1.0]
-        position *= u_aspect;                               // correct aspect ratio
-
-        color *= max(1.0 - distance(position, vec2(0.0, 0.0)), 0.2);
-    #endif
-
-    // // Mutate colors by screen position or time
-    // #if defined(EFFECT_COLOR_BLEED)
-    //     color += vec3(gl_FragCoord.x / u_resolution.x, 0.0, gl_FragCoord.y / u_resolution.y);
-    //     color.r += sin(u_time / 3.0);
-    // #endif
-
-    // // Mutate color by 3d noise
-    // #if defined (EFFECT_NOISE_TEXTURE)
-    //     #if defined(EFFECT_NOISE_ANIMATABLE) && defined(EFFECT_NOISE_ANIMATED)
-    //         color *= (abs(cnoise((v_position_world.xyz + vec3(u_time * 5., u_time * 7.5, u_time * 10.)) / 10.0)) / 4.0) + 0.75;
-    //     #endif
-    //     #ifndef EFFECT_NOISE_ANIMATABLE
-    //         color *= (abs(cnoise(v_position_world.xyz / 10.0)) / 4.0) + 0.75;
-    //     #endif
-    // #endif
 
     gl_FragColor = vec4(color, 1.0);
 }
