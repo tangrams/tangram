578a13864fdaa5565c55aa9eceea4c1f76e6e502
Peter Richardson <github@zoomy.net>
2014-08-07 02:05:07 +0000

    texture coordinate experiments

--- src/gl/shaders/polygon_fragment.glsl
+++ src/gl/shaders/polygon_fragment.glsl
@@ -3,6 +3,8 @@ uniform vec2 u_aspect;
 uniform mat4 u_meter_view;
 uniform float u_meters_per_pixel;
 uniform float u_time;
+uniform float u_test;
+uniform float u_test2;
 
 varying vec3 v_color;
 varying vec4 v_position_world;
@@ -33,24 +35,59 @@ void main (void) {
     #if defined(LIGHTING_ENVIRONMENT)
 
         // approximate location of eye (TODO: make this configurable)
-        vec3 view_pos = vec3(0., 0., 300. * u_meters_per_pixel); 
+        vec3 view_pos = vec3(0., 0., 1300. * u_meters_per_pixel); 
+        // vec3 view_pos = vec3(- u_resolution.x / 2., - u_resolution.y / 2., 1300. * u_meters_per_pixel); 
         // e = normalized vector from eye to vertex
-        vec3 e = normalize(v_position.xyz - view_pos.xyz);
+
+        // vec3 e = v_position.xyz - view_pos.xyz; // unnormalized - bright green yellow red black quadrants
+				vec3 e = normalize(v_position.xyz - view_pos.xyz); // original
+					// dark green, red, black, with a crossing in the middle
+					// i suspect this - can this be offset to start in the lower left?
+					// the trouble seems to be with v_position.xyz;
+					// view_pos.xyz is just an offset - moves the center left-down or up-right
+				// vec3 e = normalize(v_position.xyz - (v_position.xyz / u_test)); // doesn't help
+				// vec3 e = view_pos;
+
         // e.z = -abs(e.z);
         // if (e.z > 0.) {
         //     e.z = 0.;
         // }
 
-        vec3 r = reflect( e, v_normal );
+
+        vec3 r = reflect( e, v_normal ); // original
+        // vec3 r = reflect( e, normalize(v_normal) );
+				// 2. = scale modifier
+        // float m = 2. * sqrt( 
+            // pow( r.x, 2. ) + 
+            // pow( r.y, 2. ) + 
+            // pow( r.z + 1., 2. ) 
+        // );
         float m = 2. * sqrt( 
             pow( r.x, 2. ) + 
             pow( r.y, 2. ) + 
-            pow( r.z + 1., 2. ) 
+            pow( r.z + 1., u_test2 ) 
         );
 
-        vec2 texCoord = r.xy / m + .5;
+				// m is, roughly, scale - the +.5 is an offset
+				// if m is 1, the texture stretches the whole width of the sceen
+				// the + .5 seems to be in units of texure width - so .5 is half the width of the texture at its current scale
+        vec2 texCoord = r.xy / m + .5; // original
+        // vec2 texCoord = r.xy / u_test;
+        // vec2 texCoord = r.xy / u_test + u_test2;
+        
+        color = texture2D( u_envMap, texCoord).rgb; // original
+        // color = vec3(.0, texCoord); // light blues and greens - visible problem
+        // color = vec3(.0, r.xy); // dark blues and greens - no visible problem
+        // color = vec3(m); // black and white - no visible problem
+        // color = e;
+				// color = v_position.xyz; // cmyk quadrants
+				// color = normalize(v_position.xyz); // green/red/black circluar fade
+				// color = view_pos.xyz; // solid blue
+				// color = normalize(view_pos.xyz); // still solid blue
+				// color = v_normal; // 
 
-        color = texture2D( u_envMap, texCoord).rgb;
+				
+        // color = r;
 
     #endif
 
