e567799b9c5d73f86def28070fc775d89b5d45e1
Brett Camper <bcamper@alum.mit.edu>
2014-09-21 00:25:01 -0400

    feature selection: set w coord to 1 when short-circuiting selection buffer rendering

--- src/gl/shaders/polygon_vertex.glsl
+++ src/gl/shaders/polygon_vertex.glsl
@@ -62,7 +62,7 @@ void main() {
             // Discard by forcing invalid triangle if we're in the feature
             // selection pass but have no selection info
             // TODO: in some cases we may actually want non-selectable features to occlude selectable ones?
-            gl_Position = vec4(0.);
+            gl_Position = vec4(0., 0., 0., 1.);
             return;
         }
         v_selection_color = a_selection_color;
