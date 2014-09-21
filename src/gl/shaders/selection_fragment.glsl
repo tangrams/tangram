d1013bb31bddb6e9d8c8e346a216ffedc311083b
Brett Camper <bcamper@alum.mit.edu>
2014-09-21 00:27:13 -0400

    feature selection: fix vector type for no-op selection buffer render

--- src/gl/shaders/selection_fragment.glsl
+++ src/gl/shaders/selection_fragment.glsl
@@ -6,6 +6,6 @@ void main (void) {
     #if defined(FEATURE_SELECTION)
         gl_FragColor = v_selection_color;
     #else
-        gl_FragColor = vec3(0., 0., 0., 1.);
+        gl_FragColor = vec4(0., 0., 0., 1.);
     #endif
 }
