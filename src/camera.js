c7cc1fe2f5184a787f67f8924956bf23e8def72a
Brett Camper <bcamper@alum.mit.edu>
2014-10-02 11:54:57 -0400

    camera: add support for arbitrary vanishing points in perspective camera

--- src/camera.js
+++ src/camera.js
@@ -36,24 +36,39 @@ export default class Camera {
 }
 
 // Classic perspective matrix projection
-export class PerspectiveCamera extends Camera {
+// This is a specialized perspective camera that, given a desired camera focal length (which can also vary by zoom level),
+// constrains the camera height above the ground plane such that the displayed ground area of the map matches that of
+// a traditional web mercator map. This means you can set the camera location by [lat, lng, zoom] as you would a typical
+// web mercator map, then adjust the focal length as needed.
+// Vanishing point can also be adjusted to achieve different "viewing angles", while likewise preserving the area displayed
+// on the ground plane.
+// Vanishing point can also be adjusted to achieve different "viewing angles", e.g. instead of looking straight down into
+// the center of the viewport, the camera appears to be tilted at an angle. For example:
+// [0, 0] = looking towards center of screen
+// [-1, -1] = looking at lower-left corner of screen
+// [1, 0] = looking at center-right side of screen
+class PerspectiveCamera extends Camera {
 
     constructor(scene, options = {}) {
         super(scene);
         this.focal_length = options.focal_length || 2.5;
-        this.computed_focal_length = null;
+        this.vanishing_point = options.vanishing_point || { x: 0, y: 0 };
+        if (this.vanishing_point.length === 2) {
+            this.vanishing_point = { x: this.vanishing_point[0], y: this.vanishing_point[1] }; // allow vanishingpoint to also be passed as 2-elem array
+        }
+
         this.height = null;
+        this.computed_focal_length = null;
         this.perspective_mat = mat4.create();
 
+        // 'camera' is the name of the shader transform, e.g. determines where in the shader this code is injected
         GLProgram.removeTransform('camera');
-        GLProgram.addTransform(
-            'camera',
+        GLProgram.addTransform('camera', `
+            uniform mat4 u_perspective;
 
-            'uniform mat4 u_perspective;',
-
-            'void cameraProjection (inout vec4 position) { \n\
-                position = u_perspective * position; \n\
-            }'
+            void cameraProjection (inout vec4 position) {
+                position = u_perspective * position;
+            }`
         );
     }
 
@@ -105,8 +120,18 @@ export class PerspectiveCamera extends Camera {
 
         mat4.perspective(this.perspective_mat, fov, aspect, znear, zfar);
 
-        // Translate geometry into the distance so that camera is appropriate level above ground
-        mat4.translate(this.perspective_mat, this.perspective_mat, vec3.fromValues(0, 0, -this.height));
+        // Adjust perspective matrix to include vanishing point skew
+        this.perspective_mat[8] = -this.vanishing_point.x; // z column of x row, e.g. factor by which z coordinate skews x coordinate
+        this.perspective_mat[9] = -this.vanishing_point.y; // z column of y row, e.g. factor by which z coordinate skews y coordinate
+
+        // Translate geometry into the distance so that camera is appropriate height above ground
+        // Additionally, adjust xy to compensate for any vanishing point skew, e.g. move geometry so that the displayed ground
+        // plane of the map matches that expected by a traditional web mercator map at this [lat, lng, zoom].
+        mat4.translate(this.perspective_mat, this.perspective_mat, vec3.fromValues(
+            meter_zoom_y/2 * aspect * -this.vanishing_point.x,
+            meter_zoom_y/2 * -this.vanishing_point.y,
+            -this.height)
+        );
     }
 
     setupProgram(gl_program) {
@@ -116,7 +141,11 @@ export class PerspectiveCamera extends Camera {
 }
 
 // Isometric-style projection
-export class IsometricCamera extends Camera {
+// Note: this is actually an "axonometric" projection, but I'm using the colloquial term isometric because it is more recognizable.
+// An isometric projection is a specific subset of axonometric projections.
+// 'axis' determines the xy skew applied to a vertex based on its z coordinate, e.g. [0, 1] axis causes buildings to be drawn
+// straight upwards on screen at their true height, [0, .5] would draw them up at half-height, [1, 0] would be sideways, etc.
+class IsometricCamera extends Camera {
 
     constructor(scene, options = {}) {
         super(scene);
@@ -127,21 +156,20 @@ export class IsometricCamera extends Camera {
 
         this.meter_view_mat = mat4.create();
 
+        // 'camera' is the name of the shader transform, e.g. determines where in the shader this code is injected
         GLProgram.removeTransform('camera');
-        GLProgram.addTransform(
-            'camera',
+        GLProgram.addTransform('camera', `
+            uniform mat4 u_meter_view;
+            uniform vec2 u_isometric_axis;
 
-            'uniform mat4 u_meter_view;',
-            'uniform vec2 u_isometric_axis;',
+            void cameraProjection (inout vec4 position) {
+                position = u_meter_view * position;
+                position.xy += position.z * u_isometric_axis;
 
-            'void cameraProjection (inout vec4 position) { \n\
-                position = u_meter_view * position; \n\
-                position.xy += position.z * u_isometric_axis; \n\
-                                                                                    \n\
-                // Reverse z for depth buffer so up is negative, \n\
-                // and scale down values so objects higher than one screen height will not get clipped \n\
-                position.z = -position.z / 100. + 1.; \n\
-            }'
+                // Reverse z for depth buffer so up is negative,
+                // and scale down values so objects higher than one screen height will not get clipped
+                position.z = -position.z / 100. + 1.;
+            }`
         );
     }
 
@@ -159,7 +187,7 @@ export class IsometricCamera extends Camera {
 }
 
 // Flat projection (e.g. just top-down, no perspective) - a degenerate isometric camera
-export class FlatCamera extends IsometricCamera {
+class FlatCamera extends IsometricCamera {
 
     update() {
         // Axis is fixed to (0, 0) for flat camera
