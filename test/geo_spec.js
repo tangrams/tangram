c16a824c9753c05a7895e77c6c43950d4711231a
Brett Camper <bcamper@alum.mit.edu>
2014-10-16 10:53:37 -0400

    add test for polygon bounding box calc

--- test/geo_spec.js
+++ test/geo_spec.js
@@ -1,3 +1,8 @@
+import chai from 'chai';
+let assert = chai.assert;
+
+import {Geo} from '../src/geo';
+
 describe('Geo', () => {
 
     describe('Geo.setTileScale(scale)', () => {});
@@ -5,5 +10,29 @@ describe('Geo', () => {
     describe('Geo.metersToLatLng(meters)', () => {});
     describe('Geo.latLngToMeters(latlng)', () => {});
     describe('Geo.transformGeometry(geometry, transformGeometry)', () => {});
+
+    describe('Geo.findBoundingBox(polygon)', () => {
+        let min = { x: 2, y: 4 };
+        let max = { x: 10, y: 20 };
+        let polygon = [[
+            [min.x * 0.75 + max.x * 0.25, min.y * 0.75 + max.y * 0.25],
+            [min.x * 0.75 + max.x * 0.25, min.y * 0.5 + max.y * 0.5],
+            [min.x * 0.25 + max.x * 0.75, min.y * 0.75 + max.y * 0.25],
+            [min.x * 0.25 + max.x * 0.75, min.y * 0.25 + max.y * 0.75],
+            [min.x * 0.4 + max.x * 0.6, min.y * 0.5 + max.y * 0.5],
+            [min.x * 0.5 + max.x * 0.5, min.y * 0.25 + max.y * 0.75],
+            [min.x * 0.75 + max.x * 0.25, min.y * 0.25 + max.y * 0.75],
+            [min.x * 0.75 + max.x * 0.25, min.y * 0.4 + max.y * 0.6]
+        ]];
+
+        let bbox;
+        beforeEach(() => {
+            bbox = Geo.findBoundingBox(polygon);
+        });
+
+        it('calculates the expected bounding box', () => {
+            assert.deepEqual(bbox, [4, 8, 8, 16]);
+        });
+    });
 
 });
