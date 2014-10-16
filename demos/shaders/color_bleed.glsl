a6e7ea9472e4116e3adedf089ad09e55fc3ae7d6
Peter Richardson <github@zoomy.net>
2014-10-16 11:41:06 -0400

    convert colors to vec4

--- demos/shaders/color_bleed.glsl
+++ demos/shaders/color_bleed.glsl
@@ -1,4 +1,4 @@
-color += lighting * vec3(gl_FragCoord.x / u_resolution.x, 0.0, gl_FragCoord.y / u_resolution.y);
+color += lighting * vec4(gl_FragCoord.x / u_resolution.x, 0.0, gl_FragCoord.y / u_resolution.y, 1.);
 #if defined(EFFECT_COLOR_BLEED_ANIMATED)
     color.r += lighting.r * sin(u_time / 3.0);
 #endif
