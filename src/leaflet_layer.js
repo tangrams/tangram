2f0da76fb1353e70b3186d144768a281ef0ee063
Brett Camper <bcamper@alum.mit.edu>
2014-10-17 23:03:17 -0400

    leaflet layer: unhook leaflet events and destroy tangram scene when layer is removed

--- src/leaflet_layer.js
+++ src/leaflet_layer.js
@@ -13,47 +13,55 @@ export var LeafletLayer = L.GridLayer.extend({
 
         this.scene.debug = this.options.debug;
         this.scene.continuous_animation = false; // set to true for animatinos, etc. (eventually will be automated)
+        this.hooks = {};
     },
 
     // Finish initializing scene and setup events when layer is added to map
     onAdd: function (map) {
 
-        this.on('tileunload', (event) => {
+        this.hooks.tileunload = (event) => {
             var tile = event.tile;
             var key = tile.getAttribute('data-tile-key');
             this.scene.removeTile(key);
-        });
+        };
+        this.on('tileunload', this.hooks.tileunload);
 
-        this._map.on('resize', () => {
+        this.hooks.resize = () => {
             var size = this._map.getSize();
             this.scene.resizeMap(size.x, size.y);
             this.updateBounds();
-        });
+        };
+        this._map.on('resize', this.hooks.resize);
 
-        this._map.on('move',  () => {
+        this.hooks.move = () => {
             var center = this._map.getCenter();
             this.scene.setCenter(center.lng, center.lat);
             this.updateBounds();
-        });
+        };
+        this._map.on('move', this.hooks.move);
 
-        this._map.on('zoomstart', () => {
+        this.hooks.zoomstart = () => {
             console.log("map.zoomstart " + this._map.getZoom());
             this.scene.startZoom();
-        });
+        };
+        this._map.on('zoomstart', this.hooks.zoomstart);
 
-        this._map.on('zoomend',  () => {
+        this.hooks.zoomend = () => {
             console.log("map.zoomend " + this._map.getZoom());
             this.scene.setZoom(this._map.getZoom());
             this.updateBounds();
-        });
+        };
+        this._map.on('zoomend', this.hooks.zoomend);
 
-        this._map.on('dragstart',  () => {
+        this.hooks.dragstart = () => {
             this.scene.panning = true;
-        });
+        };
+        this._map.on('dragstart', this.hooks.dragstart);
 
-        this._map.on('dragend', () => {
+        this.hooks.dragend = () => {
             this.scene.panning = false;
-        });
+        };
+        this._map.on('dragend', this.hooks.dragend);
 
         // Canvas element will be inserted after map container (leaflet transforms shouldn't be applied to the GL canvas)
         // TODO: find a better way to deal with this? right now GL map only renders correctly as the bottom layer
@@ -75,7 +83,19 @@ export var LeafletLayer = L.GridLayer.extend({
 
     onRemove: function (map) {
         L.GridLayer.prototype.onRemove.apply(this, arguments);
-        // TODO: remove event handlers, destroy map
+
+        this.off('tileunload', this.hooks.tileunload);
+        this._map.off('resize', this.hooks.resize);
+        this._map.off('move', this.hooks.move);
+        this._map.off('zoomstart', this.hooks.zoomstart);
+        this._map.off('zoomend', this.hooks.zoomend);
+        this._map.off('dragstart', this.hooks.dragstart);
+        this._map.off('dragend', this.hooks.dragend);
+        this.hooks = {};
+
+        this.scene.destroy();
+        this.scene = null;
+        // TODO: allow layer to be re-added later, requiring scene re-initialization?
     },
 
     createTile: function (coords, done) {
@@ -90,6 +110,9 @@ export var LeafletLayer = L.GridLayer.extend({
     },
 
     render: function () {
+        if (!this.scene) {
+            return;
+        }
         this.scene.render();
     }
 
