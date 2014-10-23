dd7f5a05a51955386cdf6a213deb0fdd438ccb07
Peter Richardson <github@zoomy.net>
2014-10-23 17:52:27 +0100

    updated sprites

--- src/gl/shaders/point_fragment.glsl
+++ src/gl/shaders/point_fragment.glsl
@@ -1,16 +1,19 @@
 uniform vec2 u_resolution;
 uniform float u_time;
 
+uniform sampler2D u_texture; // built-in uniform for texture maps
+
 varying vec3 v_color;
 varying vec2 v_texcoord;
-
-#pragma tangram: globals
 
 void main (void) {
     vec3 color = v_color;
     vec3 lighting = vec3(1.);
 
-    //Simple threshold at dot radius
+
+    // Can we make this a define?
+
+    // Simple threshold at dot radius
     // vec2 uv = v_texcoord * 2. - 1.;
     // float len = length(uv);
     // if (len > 1.) {
@@ -18,8 +21,6 @@ void main (void) {
     // }
     // color *= (1. - smoothstep(.25, 1., len)) + 0.5;
 
-    vec2 texcoord = v_texcoord;
-    
     #pragma tangram: fragment
 
     gl_FragColor = vec4(color, 1.);
