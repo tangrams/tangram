66606c717bee089cbcd9f48d004d29dfd25b75e9
Brett Camper <bcamper@alum.mit.edu>
2014-11-25 17:56:04 -0500

    add lib back to karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function (config) {
             'node_modules/lodash/lodash.js',
             {pattern: 'test/fixtures/*', watched: false, included: false, served: true},
             './demos/lib/leaflet/leaflet.js',
+            'dist/tangram.debug.js',
             'dist/tangram.test.js'
         ],
         exclude: [  ],
