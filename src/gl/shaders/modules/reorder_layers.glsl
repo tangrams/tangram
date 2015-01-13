ad0f359c5864d4d74b35edea5905830c8a29ae9c
Brett Camper <bcamper@alum.mit.edu>
2015-01-13 18:24:09 -0500

    remove extraneous depth modification

--- src/gl/shaders/modules/reorder_layers.glsl
+++ src/gl/shaders/modules/reorder_layers.glsl
@@ -3,7 +3,6 @@
 void reorderLayers (float layer, float num_layers, inout vec4 position) {
     float layer_order = ((layer + 1.) / (num_layers + 1.)) + 1.;
     position.z /= layer_order;
-    position.xyw *= layer_order;
 }
 
 #pragma glslify: export(reorderLayers)
