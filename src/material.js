007a15cfb4832a0399e2f9f06bcd2dfa2b172f7e
Brett Camper <bcamper@alum.mit.edu>
2015-03-04 19:22:05 -0500

    materials can use CSS-style colors

--- src/material.js
+++ src/material.js
@@ -1,14 +1,40 @@
 /*global Material */
 import shaderSources from './gl/shader_sources'; // built-in shaders
 import GLSL from './gl/glsl';
+import {StyleParser} from './styles/style_parser';
 
 export default class Material {
     constructor (config) {
         config = config || {};
-        this.emission = GLSL.expandVec4(config.emission);
-        this.ambient = GLSL.expandVec4(config.ambient != null ? config.ambient : 1);
-        this.diffuse = GLSL.expandVec4(config.diffuse != null ? config.diffuse : 1);
-        this.specular = GLSL.expandVec4(config.specular);
+
+        if (config.emission == null || typeof config.emission === 'number') {
+            this.emission = GLSL.expandVec4(config.emission || 0);
+        }
+        else {
+            this.emission = StyleParser.parseColor(config.emission);
+        }
+
+        if (config.ambient == null || typeof config.ambient === 'number') {
+            this.ambient = GLSL.expandVec4(config.ambient != null ? config.ambient : 1);
+        }
+        else {
+            this.ambient = StyleParser.parseColor(config.ambient);
+        }
+
+        if (config.diffuse == null || typeof config.diffuse === 'number') {
+            this.diffuse = GLSL.expandVec4(config.diffuse != null ? config.diffuse : 1);
+        }
+        else {
+            this.diffuse = StyleParser.parseColor(config.diffuse);
+        }
+
+        if (config.specular == null || typeof config.specular === 'number') {
+            this.specular = GLSL.expandVec4(config.specular || 0);
+        }
+        else {
+            this.specular = StyleParser.parseColor(config.specular);
+        }
+
         this.shininess = config.shininess ? parseFloat(config.shininess) : 0.2;
     }
 
