ff7eed19dde31a079cb06411ce4e69305da8c147
Brett Camper <bcamper@alum.mit.edu>
2015-04-29 17:07:35 -0400

    always move point labels into tile bounds

--- src/styles/text/label_point.js
+++ src/styles/text/label_point.js
@@ -29,19 +29,19 @@ export default class LabelPoint extends Label {
         let width = this.bbox[2] - this.bbox[0];
         let height = -this.bbox[3] - (-this.bbox[1]);
 
-        // Move point labels to tile edges (only if mostly in this tile)
-        if (this.position[0] - width/2 < 0 && this.position[0] > 0) {
+        // Move point labels to tile edges
+        if (this.position[0] - width/2 < 0) {
             this.position[0] = width/2 + 1;
         }
-        else if (this.position[0] + width/2 > Geo.tile_scale && this.position[0] < Geo.tile_scale) {
+        else if (this.position[0] + width/2 > Geo.tile_scale) {
             this.position[0] = Geo.tile_scale - (width/2 + 1);
         }
 
         this.position[1] *= -1; // just doing this so Y coord is positive
-        if (this.position[1] - height/2 < 0 && this.position[1] > 0) {
+        if (this.position[1] - height/2 < 0) { // && this.position[1] > 0) {
             this.position[1] = height/2 + 1;
         }
-        else if (this.position[1] + height/2 > Geo.tile_scale && this.position[1] < Geo.tile_scale) {
+        else if (this.position[1] + height/2 > Geo.tile_scale) {
             this.position[1] = Geo.tile_scale - (height/2 + 1);
         }
         this.position[1] *= -1;
