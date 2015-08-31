34e8ac7e53c10fdb94a02882919012646426de96
Karim Naaji <karim.naaji@gmail.com>
2015-08-31 11:40:29 -0400

    Revert exceed default to 80

--- src/styles/text/label_options.js
+++ src/styles/text/label_options.js
@@ -5,7 +5,7 @@ export default class LabelOptions {
         this.keep_in_tile = (keep_in_tile != null) ? keep_in_tile : true;
         this.move_in_tile = (move_in_tile != null) ? move_in_tile : true;
         this.offset = offset || [0, 0];
-        this.line_exceed = (line_exceed != null) ? line_exceed : 30;
+        this.line_exceed = (line_exceed != null) ? line_exceed : 80;
     }
 
 }
