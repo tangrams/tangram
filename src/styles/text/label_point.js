83da88844c99b97626844ef5d32fa98a588b5cdf
Brett Camper <bcamper@alum.mit.edu>
2015-09-04 15:52:31 -0400

    point labels: fix move-in-tile to account for full bounding box including offset, etc.

--- src/styles/text/label_point.js
+++ src/styles/text/label_point.js
@@ -28,28 +28,33 @@ export default class LabelPoint extends Label {
         return aabb;
     }
 
-    moveInTile (in_tile) {
-        let width = this.aabb[2] - this.aabb[0];
-        let height = -this.aabb[3] - (-this.aabb[1]);
+    moveInTile () {
+        let updated = false;
 
-        // Move point labels to tile edges
-        if (this.position[0] - width/2 < 0) {
-            this.position[0] = width/2 + 1;
-        }
-        else if (this.position[0] + width/2 > Geo.tile_scale) {
-            this.position[0] = Geo.tile_scale - (width/2 + 1);
+        if (this.aabb[0] < 0) {
+            this.position[0] += -this.aabb[0];
+            updated = true;
         }
 
-        this.position[1] *= -1; // just doing this so Y coord is positive
-        if (this.position[1] - height/2 < 0) { // && this.position[1] > 0) {
-            this.position[1] = height/2 + 1;
+        if (this.aabb[2] >= Geo.tile_scale) {
+            this.position[0] -= this.aabb[2] - Geo.tile_scale + 1;
+            updated = true;
         }
-        else if (this.position[1] + height/2 > Geo.tile_scale) {
-            this.position[1] = Geo.tile_scale - (height/2 + 1);
-        }
-        this.position[1] *= -1;
 
-        this.aabb = this.computeAABB();
+        if (this.aabb[3] > 0) {
+            this.position[1] -= this.aabb[3];
+            updated = true;
+        }
+
+        if (this.aabb[1] <= -Geo.tile_scale) {
+            this.position[1] -= this.aabb[1] + Geo.tile_scale - 1;
+            updated = true;
+        }
+
+        if (updated) {
+            this.aabb = this.computeAABB();
+        }
+
         return !this.inTileBounds();
     }
 
