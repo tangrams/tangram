735e69829de15c6f000295f7562b1600f15d6a09
Brett Camper <bcamper@alum.mit.edu>
2015-11-13 16:52:43 -0500

    lint!

--- src/utils/version.js
+++ src/utils/version.js
@@ -1,6 +1,6 @@
 var version;
 export default version = {
-    get string() { return `v${version.major}.${version.minor}.${version.patch}` },
+    get string() { return `v${version.major}.${version.minor}.${version.patch}`; },
     major: 0,
     minor: 4,
     patch: 1,
