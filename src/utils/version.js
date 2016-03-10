e1c44c4d906f13de869c278371e6b93ee4277e1a
Brett Camper <bcamper@alum.mit.edu>
2016-03-10 14:13:47 -0500

    v0.6.0

--- src/utils/version.js
+++ src/utils/version.js
@@ -2,7 +2,7 @@ var version;
 export default version = {
     get string() { return `v${version.major}.${version.minor}.${version.patch}`; },
     major: 0,
-    minor: 5,
-    patch: 1,
+    minor: 6,
+    patch: 0,
     pre: false
 };
