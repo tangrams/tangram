18d79786c4d3fccc24be967a6058d2fdb74c04b1
Brett Camper <bcamper@alum.mit.edu>
2016-06-10 15:34:26 -0400

    deprecate 'inline object' textures in the scene file (URL-only textures w/defaults can still be defined inline, as strings) removing inline object textures clarifies things by removing 1 of 3 different texture definition scenarios, and simplifies the (currently incomplete) ES implementation

--- src/scene_loader.js
+++ src/scene_loader.js
@@ -93,56 +93,44 @@ export default SceneLoader = {
     normalizeTextures(config, path) {
         config.textures = config.textures || {};
 
-        // Move "locally defined" textures, e.g. those specified as strings or blocks,
-        // to the scene's top-level set of textures (config.textures). There are 3 such types of textures:
-        // - in a style's `texture` property (texture can be specified as string or object)
-        // - in a style's `material` properties (texture can be specified as string or object)
-        // - in a style's custom uniforms (`shaders.uniforms`) (texture can be specified as string only)
-        //
-        // For textures specified as strings, we first check to see if there is a texture already defined
-        // with that string name. If there IS, then no changes are made. If there is NOT, then the texture string
-        // is assumed to be a URL. A top-level `config.textures` object is created for the texture (with a private,
-        // synthetic texture name); the texture's URL will be expanded to include the current scene file's path below.
-        if (config.styles) {
-            for (let [style_name, style] of Utils.entries(config.styles)) {
-                // Style `texture`
-                if (style.texture) {
-                    let tex = style.texture;
+        // Add current scene's base path to globally defined textures
+        // Only adds path for textures with relative URLs, so textures in imported scenes get the base
+        // path of their immediate scene file
+        if (config.textures) {
+            for (let texture of Utils.values(config.textures)) {
+                if (texture.url) {
+                    texture.url = Utils.addBaseURL(texture.url, path);
+                }
+            }
+        }
 
-                    // Texture by URL (string-named texture not referencing existing texture definition)
-                    if (typeof tex === 'string' && !config.textures[tex]) {
-                        let texture_name = '__' + style_name;
-                        config.textures[texture_name] = { url: tex };
-                        style.texture = texture_name; // point style to location of texture
-                    }
-                    // Texture by object
-                    else if (typeof tex === 'object') {
-                        let texture_name = '__' + style_name;
-                        config.textures[texture_name] = tex;
-                        style.texture = texture_name; // point style to location of texture
-                    }
+        // Move "URL shortcut" textures, e.g. those specified as inline URL strings, to the scene's top-level
+        // set of textures (config.textures). There are 3 such cases of textures:
+        // - in a style's `texture` property
+        // - in a style's `material` properties
+        // - in a style's custom uniforms (`shaders.uniforms`)
+        //
+        // We first check to see if there is a texture already defined with that string name. The texture's URL
+        // is expanded to include the current scene's base path.
+        if (config.styles) {
+            for (let style of Utils.values(config.styles)) {
+                // Style `texture`
+                let tex = style.texture;
+                if (typeof tex === 'string' && !config.textures[tex]) {
+                    tex = Utils.addBaseURL(tex, path);
+                    config.textures[tex] = { url: tex };
+                    style.texture = tex;
                 }
 
                 // Material
                 if (style.material) {
                     for (let prop of ['emission', 'ambient', 'diffuse', 'specular', 'normal']) {
                         // Material property has a texture
-                        if (style.material[prop] != null && style.material[prop].texture) {
-                            let tex = style.material[prop].texture;
-
-                            // Texture by URL (string-named texture not referencing existing texture definition)
-                            if (typeof tex === 'string' &&
-                                !config.textures[tex]) {
-                                let texture_name = '__' + style_name + '_material_' + prop;
-                                config.textures[texture_name] = { url: tex };
-                                style.material[prop].texture = texture_name; // point style to location of texture
-                            }
-                            // Texture by object
-                            else if (typeof tex === 'object') {
-                                let texture_name = '__' + style_name + '_material_' + prop;
-                                config.textures[texture_name] = tex;
-                                style.material[prop].texture = texture_name; // point style to location of texture
-                            }
+                        let tex = style.material[prop] != null && style.material[prop].texture;
+                        if (typeof tex === 'string' && !config.textures[tex]) {
+                            tex = Utils.addBaseURL(tex, path);
+                            config.textures[tex] = { url: tex };
+                            style.material[prop].texture = tex;
                         }
                     }
                 }
@@ -152,23 +140,12 @@ export default SceneLoader = {
                     for (let {type, value, key, uniforms} of GLSL.parseUniforms(style.shaders.uniforms)) {
                         // Texture by URL (string-named texture not referencing existing texture definition)
                         if (type === 'sampler2D' && typeof value === 'string' && !config.textures[value]) {
-                            let texture_name = '__' + style_name + '_uniform_' + key;
-                            config.textures[texture_name] = { url: value };
-                            uniforms[key] = texture_name; // point style to location of texture
+                            let tex = Utils.addBaseURL(value, path);
+                            config.textures[tex] = { url: tex };
+                            uniforms[key] = tex;
                         }
                     }
 
-                }
-            }
-        }
-
-        // Add current scene file base path to textures
-        // Only adds path for textures with relative URLs, so textures in imported scenes get the base
-        // path of their immediate scene file
-        if (config.textures) {
-            for (let texture of Utils.values(config.textures)) {
-                if (texture.url) {
-                    texture.url = Utils.addBaseURL(texture.url, path);
                 }
             }
         }
