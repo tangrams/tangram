75ba9bd1209c465520f39650e28f1093b79a9be3
Brett Camper <bcamper@alum.mit.edu>
2016-06-27 18:01:13 -0400

    Revert "on scene destroy, tiles should be destroyed before textures to prevent dangling reference counts"
    This reverts commit 665e6c309e3e3747adc8f0355455e9bb6ff68ff6.

--- src/scene.js
+++ src/scene.js
@@ -202,8 +202,6 @@ export default class Scene {
             this.selection.destroy();
         }
 
-        this.tile_manager.destroy();
-
         if (this.gl) {
             Texture.destroy(this.gl);
             StyleManager.destroy(this.gl);
@@ -228,6 +226,8 @@ export default class Scene {
             });
             this.workers = null;
         }
+
+        this.tile_manager.destroy();
     }
 
     createCanvas() {
