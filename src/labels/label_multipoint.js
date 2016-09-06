a493e701054437e0e85924587f67a2c663825ad4
David Valdman <dmvaldman@gmail.com>
2016-09-06 17:59:51 -0400

    Add angle support

--- src/labels/label_multipoint.js
+++ src/labels/label_multipoint.js
@@ -1,15 +1,16 @@
 import LabelPoint from './label_point';
+import Vector from '../vector';
 
-let spacing = 50;
-let angled = false;
+let spacing_px = 40;
+let angled = true;
 
 export default function fit(line, size, options) {
     let labels = [];
-    let positions = getPositions(line, options);
+    let {positions, angles} = getPositionsAndAngles(line, options);
 
     for (let i = 0; i < positions.length; i++){
         let position = positions[i];
-        let angle = Math.random() * Math.PI;
+        let angle = angles[i];
 
         let label = new LabelPoint(position, size, options);
         label.angle = angle;
@@ -19,25 +20,30 @@ export default function fit(line, size, options) {
     return labels;
 }
 
-function getPositions(line, options){
+function getPositionsAndAngles(line, options){
+    let upp = options.units_per_pixel;
+    let spacing = spacing_px * upp;
+
     let length = getLineLength(line);
     let num_labels = Math.floor(length / spacing);
     let remainder = length - (num_labels - 1) * spacing;
 
     let positions = [];
+    let angles = [];
 
     let distance = 0.5 * remainder;
     for (let i = 0; i < num_labels; i++){
-        let position = interpolateLine(line, distance);
+        let {position, angle} = interpolateLine(line, distance, options);
         positions.push(position);
+        angles.push(angle);
         distance += spacing;
     }
 
-    return positions;
+    return {positions, angles};
 }
 
-function getAngles(options){
-    return 0;
+function getAngle(p, q, options){
+    return (angled) ? Math.atan2(q[0] - p[0], q[1] - p[1]) + Math.PI/2: 0;
 }
 
 function getLineLength(line){
@@ -52,13 +58,18 @@ function norm(p, q){
     return Math.sqrt(Math.pow(p[0] - q[0], 2) + Math.pow(p[1] - q[1], 2));
 }
 
-function interpolateLine(line, distance){
+function interpolateLine(line, distance, options){
     let sum = 0;
     for (let i = 0; i < line.length-1; i++){
-        sum += norm(line[i], line[i+1]);
+        let p = line[i];
+        let q = line[i+1];
+
+        sum += norm(p, q);
 
         if (sum > distance){
-            return interpolateSegment(line[i], line[i+1], sum - distance);
+            let position = interpolateSegment(p, q, sum - distance);
+            let angle = getAngle(p, q, options);
+            return {position, angle};
         }
     }
 }
