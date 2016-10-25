7aff9b8e82581c3d5dd7c858474690b8937fc9c8
Brett Camper <bcamper@alum.mit.edu>
2016-10-25 16:51:44 -0400

    reduce places where we pass italic info through

--- src/styles/text/canvas_text.js
+++ src/styles/text/canvas_text.js
@@ -56,7 +56,6 @@ export default class CanvasText {
                     }
 
                     text_info.space_width = space_width;
-                    text_info.italic = (text_settings.style !== 'normal');
 
                     if (text_settings.can_articulate){
                         let segments = splitLabelText(text);
