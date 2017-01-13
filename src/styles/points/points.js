ddf041bc3a241cef544aba35de6cb72bd9f40771
Brett Camper <bcamper@alum.mit.edu>
2017-01-13 10:36:00 -0500

    point outline: be more explicit about when vertex attributes are added

--- src/styles/points/points.js
+++ src/styles/points/points.js
@@ -579,10 +579,9 @@ Object.assign(Points, {
         // color
         this.fillVertexTemplate('a_color', Vector.mult(color, 255), { size: 4 });
 
-        // border
-        if (!this.texture) {
-            let outline_color = style.outline_color || StyleParser.default.outline_color;
-            this.defines.TANGRAM_POINT_OUTLINE = true;
+        // outline
+        if (this.defines.TANGRAM_POINT_OUTLINE) {
+            let outline_color = style.outline_color || StyleParser.defaults.outline_color;
             this.fillVertexTemplate('a_outline_color', Vector.mult(outline_color, 255), { size: 4 });
             this.fillVertexTemplate('a_outline_edge', style.outline_width || StyleParser.defaults.outline_width, { size: 1 });
         }
