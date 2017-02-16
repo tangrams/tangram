841165ca65c6707824dd53e541496f28433ea502
David Valdman <dmvaldman@gmail.com>
2017-02-16 15:54:19 -0500

    Cleanups

--- build/quine.js
+++ build/quine.js
@@ -1,8 +1,9 @@
 var through = require('through2');
 
-var source_variable = '__worker_src__';
-var source_origin = '__worker_src_origin__';
+var source_variable = '__worker_src__'; // variable name used to refer to the Tangram source code
+var source_origin = '__worker_src_origin__'; // variable name used to refer to URL origin of source code (e.g., "http://localhost:8000/dist/tangram.debug.js")
 
+// prepend this code - wraps Tangram source in a function and later calls arguments.callee.toString to get source
 var prefix = '(function(){';
     prefix += 'var target = (typeof module !== "undefined" && module.exports) || (typeof window !== "undefined");';
     prefix += 'if (target) {';
@@ -10,10 +11,12 @@ var prefix = '(function(){';
     prefix += 'var ' + source_origin + ' = document.currentScript.src;';
     prefix += '};';
 
+
+// append the function closing
 var postfix = '})();';
 
+// export a Browserify plugin
 module.exports = function (browserify, opts) {
-    // export a Browserify plugin
     browserify.on("bundle", function(){
         var prefixed = false;
         var sourceMap = '';
@@ -26,6 +29,7 @@ module.exports = function (browserify, opts) {
 
             var str = buf.toString();
 
+            // find the source map to push to the end of the file, rather than appearing within the function
             var match = str.match('\n//# sourceMappingURL=.*');
             if (match && match.length > 0) {
                 sourceMap = match[0];
@@ -36,8 +40,8 @@ module.exports = function (browserify, opts) {
 
             next();
         }, function(next){
-            this.push(postfix);
-            this.push(sourceMap);
+            this.push(postfix); // push the function closing
+            this.push(sourceMap); // push the source map comment
             next();
         });
 
