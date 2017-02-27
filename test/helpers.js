8c10115128ec2d0e99b32b5148a88c5d275a8f5b
Brett Camper <bcamper@alum.mit.edu>
2017-02-27 11:54:25 -0500

    tests: load worker from server instead of blob URL

--- test/helpers.js
+++ test/helpers.js
@@ -9,30 +9,8 @@ import * as URLs from '../src/utils/urls';
     - Stub the worker so that we only load it once, to avoid flooding connections (was causing disconnnect errors).
 */
 
-let worker_url = '/tangram.debug.js';
-
-function loadWorkerContent(url) {
-    let xhr = new XMLHttpRequest(), response;
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            response = xhr.responseText;
-        }
-    };
-
-    xhr.onerror = (error) => {
-        throw error;
-    };
-
-    xhr.open('GET', url, false);
-    xhr.send();
-    return response;
-}
-
-let workerBody = loadWorkerContent(worker_url);
-
 sinon.stub(Scene.prototype, 'getWorkerUrl').returns(
-    URL.createObjectURL(new Blob([workerBody], { type: 'application/javascript' }))
+    'http://localhost:9876/tangram.debug.js'
 );
 
 sinon.stub(URLs, 'revokeObjectURL').returns(null);
