6f22f97953335e77886a8c367fde703eba6f3699
Brett Camper <bcamper@alum.mit.edu>
2017-03-23 17:02:07 -0400

    polyfill Math.hypot()

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -27,3 +27,18 @@ if (typeof Object.assign !== 'function') {
         };
     })();
 }
+
+// Math.hypot polyfill
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/hypot
+Math.hypot = Math.hypot || function() {
+    var y = 0;
+    var length = arguments.length;
+
+    for (var i = 0; i < length; i++) {
+        if (arguments[i] === Infinity || arguments[i] === -Infinity) {
+            return Infinity;
+        }
+        y += arguments[i] * arguments[i];
+    }
+    return Math.sqrt(y);
+};
