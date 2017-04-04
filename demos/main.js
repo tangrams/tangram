82ef2233d8bdcd161ca88c98247434c1a4598a98
David Valdman <dmvaldman@gmail.com>
2017-04-04 16:24:48 -0400

    Refactor gui code into separate file

--- demos/main.js
+++ demos/main.js
@@ -19,74 +19,47 @@ Enjoy!
 */
 
 (function () {
+    var scene_url = 'demos/scene.yaml', rStats_debug, url_style, postUpdate, preUpdate;
 
-    var scene_url = 'demos/scene.yaml',
-        osm_debug = false,
-        rS, url_hash, map_start_location, url_style;
-
-    getValuesFromUrl();
+    var map_start_location = [16, 40.70531887544228, -74.00976419448853]; // NYC
+    var url_hash = getValuesFromUrl();
 
     // default source, can be overriden by URL
-    var
-        map = L.map('map', {
-            maxZoom: 20,
-            zoomSnap: 0,
-            trackResize: true,
-            keyboard: false
-        }),
+    var map = L.map('map', {
+        maxZoom: 20,
+        zoomSnap: 0,
+        trackResize: true
+    });
 
-        layer = Tangram.leafletLayer({
-            scene: scene_url,
-            events: {
-                hover: onFeatureHover
-            },
-            preUpdate: preUpdate,
-            postUpdate: postUpdate,
-            // highDensityDisplay: false,
-            // webGLContextOptions: { // explicitly add/override WebGL context options
-            //     antialias: false
-            // },
-            logLevel: 'debug',
-            attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
-        });
+    var layer = Tangram.leafletLayer({
+        scene: scene_url,
+        events: {
+            hover: onFeatureHover
+        },
+        preUpdate: preUpdate,
+        postUpdate: postUpdate,
+        logLevel: 'debug',
+        attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
+    });
 
     // useful events to subscribe to
     layer.scene.subscribe({
         load: function (msg) {
             // scene was loaded
-            injectAPIKey(msg.config);
         },
         update: function (msg) {
-            // scene was updated
-            injectAPIKey(msg.config);
+            // scene updated
         },
         view_complete: function (msg) {
             // new set of map tiles was rendered
         },
         error: function (msg) {
-            // debugger;
+            // on error;
         },
         warning: function (msg) {
-            // debugger;
+            // on warning;
         }
     });
-
-    function injectAPIKey(config) {
-        if (config.global.sdk_mapzen_api_key) {
-            config.global.sdk_mapzen_api_key = 'mapzen-T3tPjn7';
-        }
-        else {
-            for (var name in config.sources) {
-                var source = config.sources[name];
-                if (source.url.search('mapzen.com')) {
-                    source.url_params = source.url_params || {};
-                    source.url_params.api_key = 'mapzen-T3tPjn7';
-                }
-            }
-        }
-    }
-
-    /***** GUI/debug controls *****/
 
     /*** URL parsing ***/
 
@@ -94,11 +67,7 @@ Enjoy!
     // #[zoom],[lat],[lng]
     // #[source],[zoom],[lat],[lng] (legacy)
     function getValuesFromUrl() {
-
-        url_hash = window.location.hash.slice(1, window.location.hash.length).split('/');
-
-        // Get location from URL
-        map_start_location = [16, 40.70531887544228, -74.00976419448853]; // NYC
+        var url_hash = window.location.hash.slice(1, window.location.hash.length).split('/');
 
         if (url_hash.length >= 3) {
             // Note: backwards compatibility with old demo links, deprecate?
@@ -119,6 +88,7 @@ Enjoy!
             });
         }
 
+        return url_hash;
     }
 
     // Put current state on URL
@@ -130,7 +100,7 @@ Enjoy!
             var center = map.getCenter();
             var url_options = [map.getZoom(), center.lat, center.lng];
 
-            if (rS) {
+            if (rStats_debug) {
                 url_options.push('rstats');
             }
 
@@ -143,9 +113,6 @@ Enjoy!
     }
 
     /*** Map ***/
-
-    window.layer = layer;
-    window.map = map;
     var scene = layer.scene;
     window.scene = scene;
 
@@ -160,7 +127,7 @@ Enjoy!
         var glS = new glStats();
         glS.fractions = []; // turn this off till we need it
 
-        rS = new rStats({
+        rStats_debug = new rStats({
             values: {
                 frame: { caption: 'Total frame time (ms)', over: 10 },
                 raf: { caption: 'Time since last rAF (ms)' },
@@ -178,287 +145,15 @@ Enjoy!
         var rSDOM = document.querySelector('.rs-base');
         rSDOM.style.bottom = '0px';
         rSDOM.style.top = 'inherit';
-    }
 
-
-    // For easier debugging access
-
-    // GUI options for rendering style/effects
-    var style_options = {
-        effect: url_style || '',
-        options: {
-            'None': '',
-            'Water animation': 'water',
-            'Elevator': 'elevator',
-            'Pop-up': 'popup',
-            'Halftone': 'halftone',
-            'Windows': 'windows',
-            'Environment Map': 'envmap',
-            'Rainbow': 'rainbow'
-        },
-        saveInitial: function() {
-            this.initial = { config: JSON.stringify(scene.config) };
-        },
-        setup: function (style) {
-            // Restore initial state
-            scene.config = JSON.parse(this.initial.config);
-
-            // Remove existing style-specific controls
-            gui.removeFolder(this.folder);
-
-            // Style-specific settings
-            if (style != '') {
-                if (this.settings[style] != null) {
-                    var settings = this.settings[style] || {};
-
-                    // Change projection if specified
-                    if (settings.camera) {
-                        scene.setActiveCamera(settings.camera);
-                    }
-
-                    // Style-specific setup function
-                    if (settings.setup) {
-                        settings.uniforms = function() {
-                            return scene.styles[style] && scene.styles[style].shaders.uniforms;
-                        };
-                        settings.state = {}; // dat.gui needs a single object to old state
-
-                        this.folder = style[0].toUpperCase() + style.slice(1); // capitalize first letter
-                        settings.folder = gui.addFolder(this.folder);
-                        settings.folder.open();
-
-                        settings.setup(style);
-
-                        if (settings.folder.__controllers.length === 0) {
-                            gui.removeFolder(this.folder);
-                        }
-                    }
-
-                    scene.config.layers.earth.fill.enabled = true; // some custom shaders may need to render earth
-                }
-                else {
-                    scene.config.layers.earth.fill.enabled = false; // don't need earth layer in default style
-                }
-            }
-
-            // Recompile/rebuild
-            scene.updateConfig();
-            updateURL();
-
-            // Force-update dat.gui
-            for (var i in gui.__controllers) {
-                gui.__controllers[i].updateDisplay();
-            }
-        },
-        settings: {
-            'water': {
-                setup: function (style) {
-                    scene.config.layers.water.draw.polygons.style = style;
-                }
-            },
-            'rainbow': {
-                setup: function (style) {
-                    scene.config.layers.earth.fill.draw.polygons.color = '#333';
-                    scene.config.layers.roads.draw.lines.color = '#777';
-                    scene.config.layers.pois.enabled = false;
-                    scene.config.layers.buildings.polygons.draw.polygons.style = style;
-                    scene.config.layers.buildings.polygons.extruded.draw.polygons.style = style;
-                }
-            },
-            'popup': {
-                setup: function (style) {
-                    scene.config.layers.buildings.polygons.extruded.draw.polygons.style = style;
-                }
-            },
-            'elevator': {
-                setup: function (style) {
-                    scene.config.layers.buildings.polygons.extruded.draw.polygons.style = style;
-                }
-            },
-            'halftone': {
-                setup: function (style) {
-                    scene.config.scene.background.color = 'black';
-
-                    var layers = scene.config.layers;
-                    layers.earth.fill.draw.polygons.style = 'halftone_polygons';
-                    layers.water.draw.polygons.style = 'halftone_polygons';
-                    layers.landuse.areas.draw.polygons.style = 'halftone_polygons';
-                    layers.buildings.polygons.draw.polygons.style = 'halftone_polygons';
-                    layers.buildings.polygons.extruded.draw.polygons.style = 'halftone_polygons';
-                    layers.buildings.polygons.draw.polygons.color = 'Style.color.pseudoRandomColor()';
-                    layers.roads.draw.lines.style = 'halftone_lines';
-                    layers.pois.enabled = false;
-
-                    var enabled_layers = ['landuse', 'water', 'roads', 'buildings'];
-                    Object.keys(layers).forEach(function(l) {
-                        if (enabled_layers.indexOf(l) === -1) {
-                            layers[l].enabled = false;
-                        }
-                    });
-                }
-            },
-            'windows': {
-                camera: 'isometric', // force isometric
-                setup: function (style) {
-                    scene.config.layers.earth.fill.draw.polygons.color = '#333';
-                    scene.config.layers.roads.draw.lines.color = '#777';
-                    scene.config.layers.pois.enabled = false;
-
-                    scene.config.layers.buildings.polygons.draw.polygons.style = style;
-                    scene.config.layers.buildings.polygons.extruded.draw.polygons.style = style;
-                    // scene.config.layers.pois.enabled = false;
-                }
-            },
-            'envmap': {
-                setup: function (style) {
-                    scene.config.layers.earth.fill.draw.polygons.color = '#333';
-                    scene.config.layers.roads.draw.lines.color = '#777';
-
-                    scene.config.layers.buildings.polygons.draw.polygons.style = style;
-                    scene.config.layers.buildings.polygons.extruded.draw.polygons.style = style;
-
-                    var envmaps = {
-                        'Sunset': 'demos/images/sunset.jpg',
-                        'Chrome': 'demos/images/LitSphere_test_02.jpg',
-                        'Matte Red': 'demos/images/matball01.jpg',
-                        'Color Wheel': 'demos/images/wheel.png'
-                    };
-
-                    this.state.envmap = envmaps['Sunset'];
-                    this.folder.add(this.state, 'envmap', envmaps).onChange(function(value) {
-                        scene.config.styles.envmap.material.emission.texture = value;
-                        scene.load(scene.config, scene.config_path);
-                    }.bind(this));
-                }
-            }
-        },
-        scaleColor: function (c, factor) { // convenience for converting between uniforms (0-1) and DAT colors (0-255)
-            if ((typeof c == 'string' || c instanceof String) && c[0].charAt(0) == "#") {
-                // convert from hex to rgb
-                var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(c);
-                c = result ? [
-                    parseInt(result[1], 16),
-                    parseInt(result[2], 16),
-                    parseInt(result[3], 16)
-                ] : null;
-            }
-            return [c[0] * factor, c[1] * factor, c[2] * factor];
+        postUpdate = function postUpdate () {
+            rStats_debug('frame').end();
+            rStats_debug('tiles').set(scene.debug.renderableTilesCount());
+            rStats_debug('buffer_size').set((scene.tile_manager.getDebugSum('buffer_size') / (1024*1024)).toFixed(2));
+            rStats_debug('geometry_count').set(scene.tile_manager.getDebugSum('geometry_count'));
+            rStats_debug('feature_count').set(scene.tile_manager.getDebugSum('feature_count'));
+            rStats_debug().update();
         }
-    };
-
-    // Create dat GUI
-    var gui = new dat.GUI({ autoPlace: true });
-    function addGUI () {
-        gui.domElement.parentNode.style.zIndex = 10000;
-        window.gui = gui;
-
-        // Add ability to remove a whole folder from DAT.gui
-        gui.removeFolder = function(name) {
-            var folder = this.__folders[name];
-            if (folder == null) {
-                return;
-            }
-
-            folder.close();
-            folder.__ul.parentNode.removeChild(folder.__ul);
-            this.__folders[name] = undefined;
-            this.onResize();
-        };
-
-        // Language selector
-        var langs = {
-            '(default)': null,
-            'English': 'en',
-            'Russian': 'ru',
-            'Japanese': 'ja',
-            'German': 'de',
-            'French': 'fr',
-            'Arabic': 'ar',
-            'Hindi': 'hi',
-            'Spanish': 'es'
-        };
-        gui.language = 'en';
-        gui.add(gui, 'language', langs).onChange(function(value) {
-            scene.config.global.language = value;
-            scene.updateConfig();
-        });
-
-        // Camera
-        var camera_types = {
-            'Flat': 'flat',
-            'Perspective': 'perspective',
-            'Isometric': 'isometric'
-        };
-        gui.camera = scene.getActiveCamera();
-        gui.add(gui, 'camera', camera_types).onChange(function(value) {
-            scene.setActiveCamera(value);
-        });
-
-        // Feature selection on hover
-        gui['feature info'] = true;
-        gui.add(gui, 'feature info');
-
-        // Take a screenshot and save to file
-        gui.screenshot = function () {
-            return scene.screenshot().then(function(screenshot) {
-                // uses FileSaver.js: https://github.com/eligrey/FileSaver.js/
-                saveAs(screenshot.blob, 'tangram-' + (+new Date()) + '.png');
-            });
-        };
-        gui.add(gui, 'screenshot');
-
-        // Take a video capture and save to file
-        if (typeof window.MediaRecorder == 'function') {
-            gui.video = function () {
-                if (!gui.video_capture) {
-                    if (scene.startVideoCapture()) {
-                        gui.video_capture = true;
-                        gui.video_button.name('stop video');
-                    }
-                }
-                else {
-                    return scene.stopVideoCapture().then(function(video) {
-                        gui.video_capture = false;
-                        gui.video_button.name('capture video');
-                        saveAs(video.blob, 'tangram-video-' + (+new Date()) + '.webm');
-                    });
-                }
-            };
-            gui.video_button = gui.add(gui, 'video');
-            gui.video_button.name('capture video');
-            gui.video_capture = false;
-        }
-
-        // Layers
-        var layer_gui = gui.addFolder('Layers');
-        var layer_controls = {};
-        Object.keys(layer.scene.config.layers).forEach(function(l) {
-            if (!layer.scene.config.layers[l]) {
-                return;
-            }
-
-            layer_controls[l] = !(layer.scene.config.layers[l].enabled == false);
-            layer_gui.
-                add(layer_controls, l).
-                onChange(function(value) {
-                    layer.scene.config.layers[l].enabled = value;
-                    layer.scene.rebuild();
-                });
-        });
-
-        // Styles
-        gui.add(style_options, 'effect', style_options.options).
-            onChange(style_options.setup.bind(style_options));
-
-        // Link to edit in OSM - alt-click
-        window.addEventListener('click', function () {
-            if (key.alt) {
-                var url = 'https://www.openstreetmap.org/edit?';
-                var center = map.getCenter();
-                url += '#map=' + map.getZoom() + '/' + center.lat + '/' + center.lng;
-                window.open(url, '_blank');
-            }
-        });
     }
 
     // Feature selection
@@ -495,7 +190,7 @@ Enjoy!
 
     // Pre-render hook
     var zoom_step = 0.03;
-    function preUpdate (will_render) {
+    preUpdate = function preUpdate (will_render) {
         // Input
         if (key.isPressed('up')) {
             map._move(map.getCenter(), map.getZoom() + zoom_step);
@@ -505,24 +200,12 @@ Enjoy!
         }
 
         // Profiling
-        if (rS) {
-            rS('fps').frame();
+        if (rStats_debug) {
+            rStats_debug('fps').frame();
             if (will_render) {
-                rS('frame').start();
+                rStats_debug('frame').start();
                 glS.start();
             }
-        }
-    }
-
-    // Post-render hook
-    function postUpdate () {
-        if (rS != null) { // rstats
-            rS('frame').end();
-            rS('tiles').set(scene.debug.renderableTilesCount());
-            rS('buffer_size').set((scene.tile_manager.getDebugSum('buffer_size') / (1024*1024)).toFixed(2));
-            rS('geometry_count').set(scene.tile_manager.getDebugSum('geometry_count'));
-            rS('feature_count').set(scene.tile_manager.getDebugSum('feature_count'));
-            rS().update();
         }
     }
 
@@ -540,21 +223,16 @@ Enjoy!
         });
         layer.addTo(map);
 
-        if (osm_debug == true) {
-            window.osm_layer =
-                L.tileLayer(
-                    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    // 'https://stamen-tiles.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.jpg',
-                    {
-                        maxZoom: 19//,
-                        // opacity: 0.5
-                    })
-                .addTo(map);
-                // .bringToFront();
-        }
-
         layer.bringToFront();
     });
 
-
+    // Link to edit in OSM - alt-click
+    window.addEventListener('click', function () {
+        if (key.alt) {
+            var url = 'https://www.openstreetmap.org/edit?';
+            var center = map.getCenter();
+            url += '#map=' + map.getZoom() + '/' + center.lat + '/' + center.lng;
+            window.open(url, '_blank');
+        }
+    });
 }());
