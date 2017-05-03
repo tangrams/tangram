67702dc54d7fd4115829269add8f010f6b0f6f31
Peter Richardson <peter@mapzen.com>
2017-05-03 10:50:09 -0400

    offsets in px only, normalize not needed

--- src/builders/polylines.js
+++ src/builders/polylines.js
@@ -29,7 +29,6 @@ const DEFAULT = {
 // Scaling factor to add precision to line texture V coordinate packed as normalized short
 const v_scale_adjust = Geo.tile_scale;
 
-// todo: offset_normalize might always be same as scaling_normalize
 export function buildPolylines (lines, width, vertex_data, vertex_template,
     {
         closed_polygon,
