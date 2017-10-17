5183eaa2a7528b47d00cbdcd82479174456497b8
Brett Camper <bcamper@alum.mit.edu>
2017-10-17 10:10:46 -0400

    merge: add array merge operator (...) for combining source and destination values into a single array

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -1,4 +1,6 @@
 // Deep/recursive merge of one or more source objects into a destination object
+const array_merge_op = '...';
+
 export default function mergeObjects (dest, ...sources) {
     for (let s=0; s < sources.length; s++) {
         let source = sources[s];
@@ -10,7 +12,8 @@ export default function mergeObjects (dest, ...sources) {
             // Recursively merge the source into the destination if it is a a non-null key/value object
             // (e.g. don't merge arrays, those are treated as scalar values; null values will overwrite/erase
             // the previous destination value)
-            if (value !== null && typeof value === 'object' && !Array.isArray(value)) {
+            let is_array = Array.isArray(value);
+            if (value !== null && typeof value === 'object' && !is_array) {
                 if (dest[key] !== null && typeof dest[key] === 'object' && !Array.isArray(dest[key])) {
                     dest[key] = mergeObjects(dest[key], value);
                 }
@@ -19,9 +22,40 @@ export default function mergeObjects (dest, ...sources) {
                 }
             }
             // Overwrite the previous destination value if the source property is: a scalar (number/string),
-            // an array, or a null value
+            // an array (unless using array merge operator below), or a null value
             else if (value !== undefined) {
-                dest[key] = value;
+                let merge_index = is_array && value.indexOf(array_merge_op); // special array merge syntax
+                if (is_array && dest[key] != null && merge_index > -1) {
+                    // If the source value includes the '...' operator, the previous destination value will be
+                    // merged into the source value in its place.
+                    //
+                    // Example 1: append values
+                    //   dest: [1, 2 3]
+                    //   source: ['...', 4, 5, 6]
+                    //   -> merge result: [1, 2, 3, 4, 5, 6]
+                    //
+                    // Example 2: insert in middle
+                    //   dest: [1, 2 3]
+                    //   source: ['a', '...', 'b']
+                    //   -> merge result: ['a', 1, 2, 3, 'b']
+                    //
+                    // Example 3: existing scalar value in destination is treated as a single-element array
+                    //   dest: 'x'
+                    //   source: [1, 2, 3, '...']
+                    //   -> merge result: [1, 2, 3, 'x']
+                    //
+                    if (!Array.isArray(dest[key])) {
+                        dest[key] = [dest[key]]; // convert destination to array
+                    }
+                    let prev = dest[key];                           // previous destination values, which will be merged
+                    dest[key] = value.slice(0);                     // copy source values
+                    let args = [merge_index, 1];                    // index at which to merge in new values (and delete operator string)
+                    Array.prototype.push.apply(args, prev);         // add previous destination values to splice arguments
+                    Array.prototype.splice.apply(dest[key], args);  // merge in previous destination values
+                }
+                else {
+                    dest[key] = value; // just overwrite previous value
+                }
             }
             // Undefined source properties are ignored
         }
