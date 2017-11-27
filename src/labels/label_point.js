b041b6870ff8736b99724ff3187d621e558012cd
Brett Camper <bcamper@alum.mit.edu>
2017-11-26 23:23:02 -0500

    WIP try moving label collision space to mercator meters (may be easier to only do this for main thread pass; interdependency on geometry coordinates makes it difficult to isolate changes in collision code)

--- src/labels/label_point.js
+++ src/labels/label_point.js
@@ -9,8 +9,12 @@ export default class LabelPoint extends Label {
     constructor (position, size, layout) {
         super(size, layout);
         this.type = 'point';
-        this.position = [position[0], position[1]];
         this.parent = this.layout.parent;
+        this.position = [position[0], position[1]];
+        this.position_original = [position[0], position[1]];
+        this.position[0] = this.position[0] / layout.units_per_meter + layout.tile_min.x;
+        this.position[1] = this.position[1] / layout.units_per_meter + layout.tile_min.y;
+
         this.update();
 
         this.start_anchor_index = 1;
@@ -48,17 +52,18 @@ export default class LabelPoint extends Label {
     }
 
     updateBBoxes () {
-        let width = (this.size[0] + this.layout.buffer[0] * 2) * this.layout.units_per_pixel * Label.epsilon;
-        let height = (this.size[1] + this.layout.buffer[1] * 2) * this.layout.units_per_pixel * Label.epsilon;
+        const upp = this.layout.meters_per_pixel;
+        let width = (this.size[0] + this.layout.buffer[0] * 2) * upp * Label.epsilon;
+        let height = (this.size[1] + this.layout.buffer[1] * 2) * upp * Label.epsilon;
 
         // fudge width value as text may overflow bounding box if it has italic, bold, etc style
         if (this.layout.italic){
-            width += 5 * this.layout.units_per_pixel;
+            width += 5 * upp;
         }
 
         let p = [
-            this.position[0] + (this.offset[0] * this.layout.units_per_pixel),
-            this.position[1] - (this.offset[1] * this.layout.units_per_pixel)
+            this.position[0] + (this.offset[0] * upp),
+            this.position[1] - (this.offset[1] * upp)
         ];
 
         this.obb = new OBB(p[0], p[1], 0, width, height);
