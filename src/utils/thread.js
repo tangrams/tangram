bbd9efc514249df16122ae3d9ce05fa51797f421
Brett Camper <bcamper@alum.mit.edu>
2018-11-11 15:10:58 -0500

    fix thread test

--- src/utils/thread.js
+++ src/utils/thread.js
@@ -3,22 +3,20 @@
 // Mark thread as main or worker
 const Thread = {};
 
-export default Thread;
-
 try {
-    if (window.document !== HTMLDocument) { // jshint ignore:line
+    if (window instanceof Window && window.document instanceof HTMLDocument) { // jshint ignore:line
         Thread.is_worker = false;
         Thread.is_main   = true;
     }
 }
-catch (e) {
-    if (self !== undefined) {
-        Thread.is_worker = true;
-        Thread.is_main   = false;
+catch(e) {
+    Thread.is_worker = true;
+    Thread.is_main   = false;
 
-        // Patch for 3rd party libs that require these globals to be present. Specifically, FontFaceObserver.
-        // Brittle solution but allows that library to load on worker threads.
-        self.window = { document: {} };
-        self.document = self.window.document;
-    }
+    // Patch for 3rd party libs that require these globals to be present. Specifically, FontFaceObserver.
+    // Brittle solution but allows that library to load on worker threads.
+    self.window = { document: {} };
+    self.document = self.window.document;
 }
+
+export default Thread;
