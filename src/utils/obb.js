5ae018d006bbdc5b9f3db13eb2e34e1dcd02d957
Brett Camper <bcamper@alum.mit.edu>
2019-03-06 15:31:35 -0500

    reduce OBB memory allocation

--- src/utils/obb.js
+++ src/utils/obb.js
@@ -1,15 +1,19 @@
 import Vector from './vector';
 
-const ZERO_AXES = [[1, 0], [0, 1]]; // re-use single allocation of axes for 0-angle
+// single-allocation, reusable objects
+const ZERO_AXES = [[1, 0], [0, 1]];
+const proj_a = [], proj_b = [];
+let d0, d1, d2, d3;
 
 export default class OBB {
 
     constructor (x, y, a, w, h) {
-        this.dimension = [w, h];
+        this.dimension = [w / 2, h / 2]; // store half-dimension as that's what's needed in calculations below
         this.angle = a;
         this.centroid = [x, y];
         this.quad = null;
-        this.axes = null;
+        this.axis_0 = null;
+        this.axis_1 = null;
 
         this.update();
     }
@@ -27,97 +31,93 @@ export default class OBB {
     getExtent () {
         // special handling to skip calculations for 0-angle
         if (this.angle === 0) {
-            return [this.quad[0][0], this.quad[0][1], this.quad[2][0], this.quad[2][1]];
+            return [this.quad[0], this.quad[1], this.quad[4], this.quad[5]];
         }
 
-        let aabb = [Infinity, Infinity, -Infinity, -Infinity];
-
-        for (let i = 0; i < 4; ++i) {
-            aabb[0] = Math.min(this.quad[i][0], aabb[0]);
-            aabb[1] = Math.min(this.quad[i][1], aabb[1]);
-            aabb[2] = Math.max(this.quad[i][0], aabb[2]);
-            aabb[3] = Math.max(this.quad[i][1], aabb[3]);
-        }
+        let aabb = [
+            Math.min(this.quad[0], this.quad[2], this.quad[4], this.quad[6]),
+            Math.min(this.quad[1], this.quad[3], this.quad[5], this.quad[7]),
+            Math.max(this.quad[0], this.quad[2], this.quad[4], this.quad[6]),
+            Math.max(this.quad[1], this.quad[3], this.quad[5], this.quad[7])
+        ];
 
         return aabb;
     }
 
     perpAxes () {
-        this.axes = [
-            // Vector.normalize(Vector.sub(this.quad[2], this.quad[3])),
-            // Vector.normalize(Vector.sub(this.quad[2], this.quad[1]))
-            Vector.normalize([this.quad[2][0] - this.quad[1][0], this.quad[2][1] - this.quad[1][1]]),
-            Vector.normalize([this.quad[2][0] - this.quad[1][0], this.quad[2][1] - this.quad[1][1]])
-        ];
+        this.axis_0 = Vector.normalize([this.quad[4] - this.quad[2], this.quad[5] - this.quad[3]]);
+        this.axis_1 = Vector.normalize([this.quad[4] - this.quad[2], this.quad[5] - this.quad[3]]);
     }
 
     update () {
         // special handling to skip calculations for 0-angle
         if (this.angle === 0) {
             const c = this.centroid;
-            const w2 = this.dimension[0] / 2;
-            const h2 = this.dimension[1] / 2;
+            const w2 = this.dimension[0];
+            const h2 = this.dimension[1];
+
             this.quad = [
-                [c[0] - w2, c[1] - h2], // lower-left
-                [c[0] + w2, c[1] - h2], // lower-right
-                [c[0] + w2, c[1] + h2], // upper-right
-                [c[0] - w2, c[1] + h2]  // upper-left
+                c[0] - w2, c[1] - h2, // lower-left
+                c[0] + w2, c[1] - h2, // lower-right
+                c[0] + w2, c[1] + h2, // upper-right
+                c[0] - w2, c[1] + h2  // upper-left
             ];
-            this.axes = ZERO_AXES;
+
+            this.axis_0 = ZERO_AXES[0];
+            this.axis_1 = ZERO_AXES[1];
         }
         // calculate axes and enclosing quad
         else {
-            let x = [ Math.cos(this.angle), Math.sin(this.angle)];
-            let y = [-Math.sin(this.angle), Math.cos(this.angle)];
+            let x0 = Math.cos(this.angle) * this.dimension[0];
+            let x1 = Math.sin(this.angle) * this.dimension[0];
 
-            x = Vector.mult(x, this.dimension[0] / 2.0);
-            y = Vector.mult(y, this.dimension[1] / 2.0);
+            let y0 = -Math.sin(this.angle) * this.dimension[1];
+            let y1 = Math.cos(this.angle) * this.dimension[1];
 
+            const c = this.centroid;
             this.quad = [
-                // Vector.sub(Vector.sub(this.centroid, x), y), // lower-left
-                // Vector.sub(Vector.add(this.centroid, x), y), // lower-right
-                // Vector.add(Vector.add(this.centroid, x), y), // upper-right
-                // Vector.add(Vector.sub(this.centroid, x), y)  // upper-left
-                [this.centroid[0] - x[0] - y[0], this.centroid[1] - x[1] - y[1]], // lower-left
-                [this.centroid[0] + x[0] - y[0], this.centroid[1] + x[1] - y[1]], // lower-right
-                [this.centroid[0] + x[0] + y[0], this.centroid[1] + x[1] + y[1]], // upper-right
-                [this.centroid[0] - x[0] + y[0], this.centroid[1] - x[1] + y[1]]  // upper-left
+                c[0] - x0 - y0, c[1] - x1 - y1, // lower-left
+                c[0] + x0 - y0, c[1] + x1 - y1, // lower-right
+                c[0] + x0 + y0, c[1] + x1 + y1, // upper-right
+                c[0] - x0 + y0, c[1] - x1 + y1  // upper-left
             ];
 
             this.perpAxes();
         }
     }
 
-    static projectToAxis (obb, axis) {
-        let min = Infinity;
-        let max = -Infinity;
-
-        let quad = obb.quad;
-
+    static projectToAxis (obb, axis, proj) {
         // for each axis, project obb quad to it and find min and max values
-        for (let i = 0; i < 4; ++i) {
-            let d =  Vector.dot(quad[i], axis);
-            min = Math.min(min, d);
-            max = Math.max(max, d);
-        }
+        let quad = obb.quad;
+        d0 = quad[0] * axis[0] + quad[1] * axis[1];
+        d1 = quad[2] * axis[0] + quad[3] * axis[1];
+        d2 = quad[4] * axis[0] + quad[5] * axis[1];
+        d3 = quad[6] * axis[0] + quad[7] * axis[1];
 
-        return [min, max];
+        proj[0] = Math.min(d0, d1, d2, d3);
+        proj[1] = Math.max(d0, d1, d2, d3);
+        return proj;
     }
 
-    static axisCollide (obb_a, obb_b, axes) {
-        for (let i = 0; i < 2; ++i) {
-            let a_proj = OBB.projectToAxis(obb_a, axes[i]);
-            let b_proj = OBB.projectToAxis(obb_b, axes[i]);
-
-            if (b_proj[0] > a_proj[1] || b_proj[1] < a_proj[0]) {
-                return false;
-            }
+    static axisCollide(obb_a, obb_b, axis_0, axis_1) {
+        OBB.projectToAxis(obb_a, axis_0, proj_a);
+        OBB.projectToAxis(obb_b, axis_0, proj_b);
+        if (proj_b[0] > proj_a[1] || proj_b[1] < proj_a[0]) {
+            return false;
         }
+
+        OBB.projectToAxis(obb_a, axis_1, proj_a);
+        OBB.projectToAxis(obb_b, axis_1, proj_b);
+        if (proj_b[0] > proj_a[1] || proj_b[1] < proj_a[0]) {
+            return false;
+        }
+
         return true;
     }
 
     static intersect(obb_a, obb_b) {
-        return OBB.axisCollide(obb_a, obb_b, obb_a.axes) && OBB.axisCollide(obb_a, obb_b, obb_b.axes);
+        return OBB.axisCollide(obb_a, obb_b, obb_a.axis_0, obb_a.axis_1) &&
+               OBB.axisCollide(obb_a, obb_b, obb_b.axis_0, obb_b.axis_1);
     }
 
 }
