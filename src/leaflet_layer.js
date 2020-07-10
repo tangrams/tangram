8416328f975f4ff424704c5bccdd3346e2fd3f8b
Brandon Liu <bdon@bdon.org>
2020-07-10 16:21:32 +0800

    If no explicit attribution in leafletLayer, use attribution in YAML sources.

--- src/leaflet_layer.js
+++ src/leaflet_layer.js
@@ -166,6 +166,12 @@ function extendLeaflet(options) {
                         blocking: false
                     }).then(() => {
 
+                    if (!this.options.attribution) {
+                        for (const [key, value] of Object.entries(this.scene.config.sources)) {
+                            if (value.attribution) map.attributionControl.addAttribution(value.attribution)
+                        }
+                    }
+
                     this._updating_tangram = true;
 
                     this.updateSize();
